14c75cce7a6e4a8234d9fce1711d3958755a2a37
SamuliAP <samuli.anola-pukkila@hotmail.fi>
2020-12-10 12:31:44 +0200

    fix shutdown test, put state expected to die

--- tests/shutdown/shutDown_test.go
+++ tests/shutdown/shutDown_test.go
@@ -13,10 +13,10 @@ func TestShutdownState(t *testing.T) {
 		return
 	}
 
-	api.PutStateHandleError(t, api.CMD_SHUTDOWN)
+	putStateErr := api.PutState(api.CMD_SHUTDOWN)
 	_, messagesErr := api.GetResponse("http://server/messages")
 	_, stateErr := api.GetResponse("http://server/state")
-	if messagesErr == nil || stateErr == nil {
+	if putStateErr == nil || messagesErr == nil || stateErr == nil {
 		t.Error("Services still running after shutdown")
 	}
 }
