14a4a26d2a838184225911b39babce70570de57e
SamuliAP <samuli.anola-pukkila@hotmail.fi>
2020-12-10 10:33:50 +0200

    improve runlog tests

--- tests/runLogAPI_test.go
+++ tests/runLogAPI_test.go
@@ -6,24 +6,30 @@ import (
 	"testing"
 )
 
-func testRunLog(t *testing.T) {
+func TestRunLog(t *testing.T) {
 
-	handleRunlogNewStateCheck(t, api.STATE_PAUSED, []string{api.STATE_PAUSED})
-	handleRunlogNewStateCheck(t, api.STATE_PAUSED, []string{api.STATE_PAUSED})
-	handleRunlogNewStateCheck(t, api.STATE_RUNNING, []string{api.STATE_RUNNING})
-	handleRunlogNewStateCheck(t, api.CMD_INIT, []string{api.CMD_INIT, api.STATE_RUNNING})
-	handleRunlogNewStateCheck(t, api.CMD_INIT, []string{api.CMD_INIT, api.STATE_RUNNING})
-	handleRunlogNewStateCheck(t, api.STATE_PAUSED, []string{api.STATE_PAUSED})
-	handleRunlogNewStateCheck(t, api.CMD_INIT, []string{api.CMD_INIT, api.STATE_RUNNING})
+	api.PutStateHandleError(t, api.STATE_RUNNING)
+	HandleRunlogNewStateCheck(t, api.STATE_PAUSED, []string{api.STATE_PAUSED})
+	HandleRunlogNewStateCheck(t, api.STATE_PAUSED, []string{})
+	HandleRunlogNewStateCheck(t, api.STATE_RUNNING, []string{api.STATE_RUNNING})
+	HandleRunlogNewStateCheck(t, api.STATE_RUNNING, []string{})
+	HandleRunlogNewStateCheck(t, api.CMD_INIT, []string{api.CMD_INIT, api.STATE_RUNNING})
+	HandleRunlogNewStateCheck(t, api.CMD_INIT, []string{api.CMD_INIT, api.STATE_RUNNING})
+	HandleRunlogNewStateCheck(t, api.STATE_PAUSED, []string{api.STATE_PAUSED})
+	HandleRunlogNewStateCheck(t, api.CMD_INIT, []string{api.CMD_INIT, api.STATE_RUNNING})
 }
 
-func handleRunlogNewStateCheck(t *testing.T, state string, contains []string) {
+func HandleRunlogNewStateCheck(t *testing.T, state string, contains []string) {
 	runLogSize := len(api.GetRunLogHandleError(t))
 
 	api.PutStateHandleError(t, state)
 	runLog := api.GetRunLogHandleError(t)
 
 	newLog := runLog[runLogSize:]
+	t.Log(newLog)
+	if len(contains) == 0 && len(newLog) != 0 {
+		t.Error("Run log logged duplicate event", state)
+	}
 	for _, c := range contains {
 		if strings.Contains(newLog, c) {
 			t.Error("Run log did not record state:", state)
