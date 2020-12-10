dfd376c2b0bff8224a41aab73f6a5fbf49540fbf
SamuliAP <samuli.anola-pukkila@hotmail.fi>
2020-12-10 02:31:34 +0200

    initial API service, GET /messages implemented

---
+++ api/api.go
@@ -0,0 +1,31 @@
+package main
+
+import (
+	"fmt"
+	"io/ioutil"
+	"net/http"
+)
+
+func GetMessages(w http.ResponseWriter, req *http.Request) {
+
+	resp, err := http.Get("http://httpserv")
+	if err != nil {
+		w.WriteHeader(http.StatusInternalServerError)
+		fmt.Fprint(w, "Couldn't read data source")
+	}
+	defer resp.Body.Close()
+
+	body, err := ioutil.ReadAll(resp.Body)
+	if err != nil {
+		w.WriteHeader(http.StatusInternalServerError)
+		fmt.Fprint(w, "Couldn't read data source")
+	}
+
+	fmt.Fprint(w, string(body))
+}
+
+func main() {
+
+	http.HandleFunc("/", GetMessages)
+	http.ListenAndServe(":80", nil)
+}
